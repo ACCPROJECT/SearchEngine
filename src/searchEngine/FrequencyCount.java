f43b013825ed343b6ad597826badfb547310f677
SaiPrasannaPolagani <85219517+SaiPrasannaPolagani@users.noreply.github.com>
2021-07-31 22:37:23 +0530

    counting words -Sai Prasanna Polagani
    counting words -Sai Prasanna Polagani

--- src/searchEngine/FrequencyCount.java
+++ src/searchEngine/FrequencyCount.java
@@ -10,26 +10,24 @@ public class FrequencyCount {
 			
 		public static void WordCount() throws IOException{
 			
-			 HashMap <String,Integer> hm = new HashMap<String,Integer>();
+			 HashMap <String,Integer> hm = new HashMap<String,Integer>(); //hashmap object to store the words
 			 BufferedReader br = null;
 			 try {
-				 
-				/* br =  new BufferedReader(new FileReader("D:\\Java Workspace\\SearchEngine\\convertedWebPages"));
-				 String str = br.readLine();*/
 				 Scanner sc= new Scanner(System.in);
-				 File myFolder = new File("D:\\Java Workspace\\SearchEngine\\convertedWebPages");
+				 File myFolder = new File("C:\\Users\\Dell\\Desktop\\searchEngine-main\\convertedWebPages"); //reading the converted text files
 				 File[] files = myFolder.listFiles();
-				 System.out.println("Enter a word to search: ");
-				 String userInput= sc.nextLine();
+				 System.out.println("Enter a word to search: "); 
+				 String userInput= sc.nextLine();                 //reading user input
 				 
 				 for (File file : files) {
-					br =  new BufferedReader(new FileReader(file));
-					String str = br.readLine();
+					br =  new BufferedReader(new FileReader(file)); 
+ 					String str = br.readLine();  					// reading the content lines in the files
+
 				 while(str!=null) {
-					 String words[]= str.toLowerCase().split(" ");
+					 String words[]= str.toLowerCase().split(" ");   // getting the words
 					 for(String word:words) {
-						 if(hm.containsKey(word)) {
-							 hm.put(word,hm.get(word)+1);
+						 if(hm.containsKey(word)) {       //search for the word in hashmap
+							 hm.put(word,hm.get(word)+1);   
 							 
 						 }
 						 else {
@@ -38,28 +36,11 @@ public class FrequencyCount {
 					 }
 					 str=br.readLine();
 				 }
-				 if(hm.containsKey(userInput))
-					 System.out.println(userInput + " repeated " +hm.get(userInput)+" times in "+ file.getName());
+				 if(hm.containsKey(userInput)) 
+					 System.out.println(userInput + " repeated " +hm.get(userInput)+" times in "+ file.getName()); 
 				 else
 					 System.out.println("Word not found in "+ file.getName());
 				 }
-				 
-//				 if(hm.containsKey(userInput))
-//					 System.out.println(userInput + " repeated " +hm.get(userInput)+" times.");
-//				 else
-//					 System.out.println("Word not found");
-//				String repeated = null;
-//				int count= 0;
-//				Set<Entry<String,Integer>> entrySet = hm.entrySet();
-//				for(Entry<String,Integer> entry:entrySet) {
-//					if(entry.getValue()>count) {
-//						repeated=entry.getKey();
-//						count=entry.getValue();
-//					}
-//				}
-				
-//				System.out.println("The word repeated for most number times is "+ repeated);
-//				System.out.println("The frequency of the occured word is "+ count);
 				 
 			 
 		}catch(Exception e) {
